b1729e7710ab54e491dba78c957ced131390345d
Stephan <sgriesel@gmail.com>
2022-03-20 11:56:33 +0100

    contenful test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
+const query = `
+{
+  pageCollection {
+    items {
+      title
+      logo {
+        url
+      }
+    }
+  }
+}
+`
 
 function App() {
+  // define the initial state
+  const [page, setPage] = useState(null);
+
+  useEffect(() => {
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/SPACEIDGOESHERE/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Authenticate the request
+          Authorization: "Bearer TOKENGOESHERE",
+        },
+        // send the GraphQL query
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+
+        // rerender the entire component with new data
+        setPage(data.pageCollection.items[0]);
+      });
+  }, []);
+
+  // show a loading screen case the data hasn't arrived yet
+  if (!page) {
+    return "Loading...";
+  }
+  //return statement and JSX template
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={page.logo.url} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {page.title}
         </p>
         <a
           className="App-link"
