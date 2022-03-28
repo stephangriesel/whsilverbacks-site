7ff4e24351f80602ae50b97ca71fbce73b42160c
Stephan <sgriesel@gmail.com>
2022-03-28 16:01:52 +0200

    update comments

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,17 +1,18 @@
 import { render, screen } from '@testing-library/react'
 import Greeting from './Greeting'
 
-test('Confirm that hello is rendered', () => {
-  // the three a's, arrange (set up test data), act (run logic) & assert (compare execution results)
+describe('Greeting Component', () => { // test suite start
+  test('Confirm that hello is rendered', () => {
+    //Arrange: Set Up Test Data
+    render(<Greeting />);
+  
+    //Act: Run Logic
+  
+  
+    //Assert: Compare execution results
+    const helloElement = screen.getByText('Hello', {exact: true});
+    expect(helloElement).toBeInTheDocument();
+  
+  });
+})
 
-  //Arrange
-  render(<Greeting />);
-
-  //Act
-
-
-  //Assert
-  const helloElement = screen.getByText('Hello', {exact: true});
-  expect(helloElement).toBeInTheDocument();
-
-});
