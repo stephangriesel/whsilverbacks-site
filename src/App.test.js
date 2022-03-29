c4bace1b2bbf4ecc6f9bbe94cff489d3feb0bfb2
Stephan <sgriesel@gmail.com>
2022-03-29 15:16:48 +0200

    dabble with integration testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,23 @@
 import { render, screen } from '@testing-library/react'
 import App from './App';
+import { mount } from '@cypress/react';
 
-describe('App component', () => {
-  test('Render something on app component so this unit test passes', () => {
-    // Arrange
-    render(<App />)
-    // Action
-    // .. do nothing
-    // Assert
-    // .. do nothing, might add later
-  })
-})
+// Unit test
+
+// describe('App component', () => {
+//   test('Render something on app component so this unit test passes', () => {
+//     // Arrange
+//     render(<App />)
+//     // Action
+//     // .. do nothing
+//     // Assert
+//     // .. do nothing, might add later
+//   })
+// })
+
+// Integration test
+
+it('renders learn react link', () => {
+  mount(<App />);
+  cy.get('a').contains('Learn React');
+});
