b186616240789e42af7d76cc6290a334a8f7cea8
Stephan <sgriesel@gmail.com>
2022-03-29 15:23:55 +0200

    remove old integration tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,15 @@
 import { render, screen } from '@testing-library/react'
 import App from './App';
-import { mount } from '@cypress/react';
 
 // Unit test
 
-// describe('App component', () => {
-//   test('Render something on app component so this unit test passes', () => {
-//     // Arrange
-//     render(<App />)
-//     // Action
-//     // .. do nothing
-//     // Assert
-//     // .. do nothing, might add later
-//   })
-// })
-
-// Integration test
-
-it('renders learn react link', () => {
-  mount(<App />);
-  cy.get('a').contains('Learn React');
-});
+describe('App component', () => {
+  test('Render something on app component so this unit test passes', () => {
+    // Arrange
+    render(<App />)
+    // Action
+    // .. do nothing
+    // Assert
+    // .. do nothing, might add later
+  })
+})
