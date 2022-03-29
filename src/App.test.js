2ac02c7f9b0666b7c59f98c46a897b2acbff2209
Stephan <sgriesel@gmail.com>
2022-03-29 14:25:46 +0200

    add unit test

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+import App from './App';
+
+describe('App component', () => {
+  test('Render something on app component so this unit test passes', () => {
+    // Arrange
+    render(<App />)
+    // Action
+    // .. do nothing
+    // Assert
+    // .. do nothing, might add later
+  })
+})
