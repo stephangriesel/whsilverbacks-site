9650da1dbccd943074da2442c60e002f7e0e3cef
Stephan <sgriesel@gmail.com>
2022-05-06 06:53:27 +0200

    remove dropdown dependency

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,32 +1,16 @@
 import React, {useRef, useState} from 'react'
-import {
-  ControlledMenu,
-  MenuItem,
-} from '@szhsin/react-menu';
-import '@szhsin/react-menu/dist/index.css';
-import '@szhsin/react-menu/dist/transitions/slide.css';
 
 // Navbar Styles
 import Nav from '../../UI/Nav/Nav'
 
 const Navbar = () => {
-  const ref = useRef(null);
-  const [isOpen, setOpen] = useState();
   return (
     <Nav>
-      <div ref={ref} className="btn" onMouseEnter={() => setOpen(true)}>
-        Hover to Open
-      </div>
-      <ControlledMenu
-        state={isOpen ? 'open' : 'closed'}
-        anchorRef={ref}
-        onMouseLeave={() => setOpen(false)}
-        onClose={() => setOpen(false)}
-      >
-        <MenuItem>Home</MenuItem>
-        <MenuItem>Projects</MenuItem>
-        <MenuItem>Contact</MenuItem>
-      </ControlledMenu>
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
     </Nav>
   )
 }
