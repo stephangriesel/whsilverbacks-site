2499a5f0caa2f8e0d236875fc80bf1c40b3fdc9f
Stephan <sgriesel@gmail.com>
2022-05-27 16:31:44 +0200

    add pages

---
+++ src/components/EventConstruction/EventConstruction.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EventConstruction = () => {
+  return (
+    <div>EventConstruction</div>
+  )
+}
+
+export default EventConstruction
---
+++ src/components/TemporaryStructures/TemporaryStructures.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TemporaryStructures = () => {
+  return (
+    <div>TemporaryStructures</div>
+  )
+}
+
+export default TemporaryStructures
