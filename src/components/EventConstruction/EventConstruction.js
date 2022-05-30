ab5ab9a81937dfcb6de6da5a9c142606f9b8377d
Stephan <sgriesel@gmail.com>
2022-05-30 07:44:11 +0200

    query set up & data render

--- src/components/EventConstruction/EventConstruction.js
+++ src/components/EventConstruction/EventConstruction.js
@@ -1,8 +1,73 @@
 import React from 'react'
 
+import { useRef, useState, useEffect } from "react"
+
+const query = `
+{
+  eventConstructionCollection {
+    items {
+      title
+      heading
+      imagesCollection {
+        items {
+          url
+        }
+      }
+      paragraph1
+      paragraph2
+      paragraph3
+    }
+  }
+}
+`
+
+
+// Environment variables
+const { REACT_APP_SPACE_ID, REACT_APP_CDA_TOKEN } = process.env;
+
+
+
 const EventConstruction = () => {
+
+  // define the initial state
+  const [eventConstruction, setEventConstruction] = useState(null);
+
+  useEffect(() => {
+    window
+      // Change to template string & use template literals to define environment variable
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/${REACT_APP_SPACE_ID}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Authenticate the request
+          Authorization: `Bearer ${REACT_APP_CDA_TOKEN}`,
+        },
+        // send the GraphQL query
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+
+        // rerender the entire component with new data
+        setEventConstruction(data.eventConstructionCollection.items[0]);
+      });
+  }, []);
+
+  // show a loading screen case the data hasn't arrived yet
+  if (!eventConstruction) {
+    return "Loading...";
+  }
+
   return (
-    <div>EventConstruction</div>
+    <div>
+      <h1>
+        {eventConstruction.title}
+      </h1>
+      <img src={eventConstruction.imagesCollection.items[0].url} alt="Events" />
+    </div>
   )
 }
 
