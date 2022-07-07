591c50aa3f5c52c03208980b1eaed4ed9ae2d25b
Stephan <sgriesel@gmail.com>
2022-07-07 06:29:11 +0200

    remove

--- src/globals/images/data.js
+++
@@ -1,4 +0,0 @@
-const AboutImage = {
-  url1: 'https://images.ctfassets.net/y2kxguv0syko/2Ze0rLaiYYD4GVIZVj1Z4i/7b50acd7c6c5793625889bff0e48c984/aboutsunrise.jpg',
-  url2: 'https://images.ctfassets.net/y2kxguv0syko/6QWe1z8B7srvIGsdpuT789/f8a47180be229a65a5dbbc37e7d7561c/Craning_Day.jpg'
-}
