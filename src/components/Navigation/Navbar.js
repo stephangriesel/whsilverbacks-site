1434e2720e5329de8b30e194cbb657ed6269a0e8
Stephan <sgriesel@gmail.com>
2022-07-13 07:59:33 +0200

    clean up & import navigations

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,37 +1,16 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react';
 
-import { Link } from 'react-router-dom';
+import MobileNavigation from './MobileNavigation';
+import Navigation from './Navigation';
 
-import Flex from '../../UI/Layout/Flex/Flex'
-import ListItem from '../../UI/List/ListItem'
-
-import './styles.css'
-
-// Navbar Styles
-import Nav from '../../UI/Nav/Nav'
+import classes from './Navbar.module.css';
 
 const Navbar = () => {
-  const [isMobile, setIsMobile] = useState(false);
   return (
-    <Nav>
-      <div className={isMobile ? "nav-links-mobile" : "nav-links"}
-      onClick={() => setIsMobile(false)}
-      >
-        <Flex>
-          <ListItem><Link to="/">HOME</Link></ListItem>
-          <ListItem><Link to="/about">ABOUT</Link></ListItem>
-          <ListItem><Link to="/eventconstruction">EVENT CONSTRUCTION</Link></ListItem>
-          <ListItem><Link to="/stretchtents">STRETCH TENTS</Link></ListItem>
-          <ListItem><Link to="/temporarystructures">TEMPORARY STRUCTURES</Link></ListItem>
-          <ListItem><Link to="/contact">CONTACT</Link></ListItem>
-        </Flex>
-        <button className='mobile-menu-icon'
-        onClick={() => setIsMobile(!isMobile)}
-        >
-          {isMobile ? <i className='fas fa-times'></i> : <i className='fas fa-bars'></i>}
-        </button>
-      </div>
-    </Nav>
+    <div className={classes.Navbar}>
+      <MobileNavigation />
+      <Navigation />
+    </div>
   )
 }
 
