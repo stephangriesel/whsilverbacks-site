3fe2929689ea9bc7fa56ea58c189c4a3ce98d6b0
Stephan <sgriesel@gmail.com>
2022-07-13 07:57:11 +0200

    add links object

---
+++ src/components/Navigation/NavLinks.js
@@ -0,0 +1,26 @@
+const NavLinks = () => {
+  return (
+    <ul>
+      <li>
+        <a href="/">HOME</a>
+      </li>
+      <li>
+        <a href="/about">ABOUT</a>
+      </li>
+      <li>
+        <a href="/eventconstruction">EVENT CONSTRUCTION</a>
+      </li>
+      <li>
+        <a href="/stretchtents">STRETCH TENTS</a>
+      </li>
+      <li>
+        <a href="/temporarystructures">TEMPORARY STRUCTURES</a>
+      </li>
+      <li>
+        <a href="/contact">CONTACT</a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavLinks;
