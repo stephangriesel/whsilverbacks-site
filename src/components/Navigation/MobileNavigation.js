4c184d26f579eb9dffcc1791cc66dfbc89cbfcd7
Stephan <sgriesel@gmail.com>
2022-07-14 06:30:13 +0200

    add icons & logic

--- src/components/Navigation/MobileNavigation.js
+++ src/components/Navigation/MobileNavigation.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import NavLinks from "./NavLinks";
 import classes from './Navbar.module.css';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {FaWindowClose} from 'react-icons/fa';
 
 const MobileNavigation = () => {
+  const [open,setOpen] = useState(false);
+
+  const hamburgerIcon = <GiHamburgerMenu className={classes.Hamburger} size='40px' color="white" onClick={() => setOpen(!open)}/>
+  const hamburgerClose = <FaWindowClose className={classes.Hamburger} size='40px' color="white" onClick={() => setOpen(!open)}/>
+
   return (
     <nav className={classes.MobileNavigation}>
-      <NavLinks />
+      {open ? hamburgerClose : hamburgerIcon}
+      {open && <NavLinks />}
     </nav>
   );
 }
