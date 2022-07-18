19fb6c2124b4a84177df107fac24f97a565a7e0a
Stephan <sgriesel@gmail.com>
2022-07-18 08:01:39 +0200

    menu animation

--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -1,24 +1,38 @@
+import { Fade } from "react-awesome-reveal";
+
 const NavLinks = (props) => {
   return (
     <ul>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
-        <a href="/">HOME</a>
-      </li>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
-        <a href="/about">ABOUT</a>
-      </li>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
-        <a href="/eventconstruction">EVENT CONSTRUCTION</a>
-      </li>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
-        <a href="/stretchtents">STRETCH TENTS</a>
-      </li>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
-        <a href="/temporarystructures">TEMPORARY STRUCTURES</a>
-      </li>
-      <li onClick={() => props.isMobile && props.closeMobileMenu()}>
-        <a href="/contact">CONTACT</a>
-      </li>
+      <Fade delay={50}>
+        <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+          <a href="/">HOME</a>
+        </li>
+      </Fade>
+      <Fade delay={70}>
+        <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+          <a href="/about">ABOUT</a>
+        </li>
+      </Fade>
+      <Fade delay={110}>
+        <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+          <a href="/eventconstruction">EVENT CONSTRUCTION</a>
+        </li>
+      </Fade>
+      <Fade delay={140}>
+        <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+          <a href="/stretchtents">STRETCH TENTS</a>
+        </li>
+      </Fade>
+      <Fade delay={170}>
+        <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+          <a href="/temporarystructures">TEMPORARY STRUCTURES</a>
+        </li>
+      </Fade>
+      <Fade delay={200}>
+        <li onClick={() => props.isMobile && props.closeMobileMenu()}>
+          <a href="/contact">CONTACT</a>
+        </li>
+      </Fade>
     </ul>
   );
 }
