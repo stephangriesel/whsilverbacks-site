bff9c3860850c35a0ca71500a8c40b4b065c965e
Stephan <sgriesel@gmail.com>
2022-07-24 09:15:39 +0200

    clean up

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -1,60 +1,11 @@
 import React from 'react'
-import { useState, useEffect } from "react"
 import FlexColumn from '../../UI/Layout/FlexColumn/FlexColumn'
 import FlexRow from '../../UI/Layout/FlexRow/FlexRow'
 import FullViewPort from '../../UI/FullViewPort/FullViewPort'
 
 import './success.css'
 
-const query = `
-{
-  contactCollection {
-    items {
-      title
-      heading
-      slotParagraph
-    }
-  }
-}
-`
-
-// Environment variables
-const { REACT_APP_SPACE_ID, REACT_APP_CDA_TOKEN } = process.env;
-
-const Contact = () => {
-
-  const [contact, setContact] = useState(null);
-
-
-  useEffect(() => {
-    window
-      // Change to template string & use template literals to define environment variable
-      .fetch(`https://graphql.contentful.com/content/v1/spaces/${REACT_APP_SPACE_ID}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          // Authenticate the request
-          Authorization: `Bearer ${REACT_APP_CDA_TOKEN}`,
-        },
-        // send the GraphQL query
-        body: JSON.stringify({ query }),
-      })
-      .then((response) => response.json())
-      .then(({ data, errors }) => {
-        if (errors) {
-          console.error(errors);
-        }
-
-        // rerender the entire component with new data
-        setContact(data.contactCollection.items[0]);
-      });
-  }, []);
-
-  // show a loading screen case the data hasn't arrived yet
-  if (!contact) {
-    return "";
-  }
-
+const Success = () => {
   return (
     <FullViewPort>
       <FlexRow>
@@ -67,4 +18,4 @@ const Contact = () => {
   )
 }
 
-export default Contact
+export default Success
