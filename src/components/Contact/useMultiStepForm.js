908fc5d32e782729ddca24809c8a771446a8a444
Stephan Griesel <sgriesel@gmail.com>
2022-10-06 09:01:18 +0200

    custom hook form

---
+++ src/components/Contact/useMultiStepForm.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const useMultiStepForm = (steps) => {
+    const [currentStepIndex, setCurrentStepIndex] = useState(0)
+
+    function next(){
+        setCurrentStepIndex(i => {
+            if (i >= steps.length - 1) return i;
+            return i + 1;
+        })
+    }
+    function back(){
+        if (i <= 0) return i;
+        return i - 1;
+    }
+    function goTo(index){
+        setCurrentStepIndex(index);
+    }
+
+  return (
+    currentStepIndex, 
+    steps[currentStepIndex],
+    goTo,
+    next,
+    back
+  )
+}
+
+export default useMultiStepForm
